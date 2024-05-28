91ae968e48bae4d821ca476582786d16e8527fe3
karnatipavan2580 <91123302+karnatipavan2580@users.noreply.github.com>
2024-05-29 01:06:47 +0530

    products by store

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Product = require("../models/product");
+const StoreProductMap = require("../models/storeProductMap");
 
 const router = express.Router();
 
@@ -83,23 +84,29 @@ router.get("/byAge", async (req, res) => {
   try {
     console.log("query req => ", req.query);
     let query = null;
-    if (req.query && req.query.ageType === "preschool") {
-      query = {
-        $or: [{ Age: "0+" }, { Age: "1+" }, { Age: "2+" }],
-      };
-    } else if (req.query && req.query.ageType === "playschool") {
-      query = {
-        $or: [{ Age: "3+" }, { Age: "4+" }, { Age: "5+" }],
-      };
-    } else if (req.query && req.query.ageType === "primaryschool") {
-      query = { $or: [{ Age: "6+" }, { Age: "8+" }] };
-    } else {
-      query = null;
-    }
+    const productsByStore = await getProductsByStore(req.query.store);
+    console.log("pro => ", productsByStore);
+    const toys_list1 = productsByStore.filter((product) => {
+      if (product.visible) {
+        if (req.query && req.query.ageType === "preschool") {
+          return (
+            product.Age === "0+" || product.Age === "1+" || product.Age === "2+"
+          );
+        } else if (req.query && req.query.ageType === "playschool") {
+          return (
+            product.Age === "3+" || product.Age === "4+" || product.Age === "5+"
+          );
+        } else if (req.query && req.query.ageType === "primaryschool") {
+          return (
+            product.Age === "6+" || product.Age === "7+" || product.Age === "8+"
+          );
+        }
+      }
+    });
     console.log("query => ", query);
-    const toys_list = await Product.find(query);
-    console.log("toys_list => ", toys_list);
-    res.json(toys_list);
+    // const toys_list = await productsByStore.find(query);
+    console.log("toys_list => ", toys_list1);
+    res.json(toys_list1);
   } catch (err) {
     console.error("Error fetching products by age:", err);
     res.status(500).send("Internal Server Error");
@@ -110,9 +117,14 @@ router.get("/byCategory/:category", async (req, res) => {
   try {
     const category = req.params.category;
     console.log("category => ", category);
-    const toys_list = await Product.find({ Category: category });
-    console.log("toys_list => ", toys_list);
-    res.json(toys_list);
+    const productsByStore = await getProductsByStore(req.query.store);
+    console.log("pro => ", productsByStore);
+    const toys_list1 = productsByStore.filter(
+      (product) => product.Category === category
+    );
+    // const toys_list = await Product.find({ Category: category });
+    console.log("toys_list => ", toys_list1);
+    res.json(toys_list1);
   } catch (err) {
     console.error("Error fetching products by category:", err);
     res.status(500).send("Internal Server Error");
@@ -121,25 +133,33 @@ router.get("/byCategory/:category", async (req, res) => {
 
 router.get("/search", async (req, res) => {
   try {
-    const { searchKey } = req.query;
+    const { searchKey, store } = req.query;
     console.log("searchKey => ", searchKey);
+    const productsByStore = await getProductsByStore(store);
+    console.log("pro => ", productsByStore);
     const regex = new RegExp(searchKey, "i");
-    const toys_list = await Product.find({
-      SearchKey: regex,
-    });
-    console.log("search toys list => ", toys_list);
-    res.json(toys_list);
+    const toys_list1 = productsByStore.filter((product) =>
+      regex.test(product.SearchKey)
+    );
+    // const toys_list = await Product.find({
+    //   SearchKey: regex,
+    // });
+    console.log("search toys list => ", toys_list1);
+    res.json(toys_list1);
   } catch (err) {
     console.error("Error fetching products with search query:", err);
     res.status(500).send("Internal Server Error");
   }
 });
 
-router.get("/get/:code", async (req, res) => {
+router.get("/get/:code/store/:storeId", async (req, res) => {
   try {
     const code = req.params.code;
-    console.log("code", code);
-    const product = await Product.findOne({ Code: code });
+    const store = req.params.storeId;
+    console.log("code", code, store);
+    const productsByStore = await getProductsByStore(store);
+    const product = productsByStore.find((product) => product.Code === code);
+    // const product = await Product.findOne({ Code: code });
     console.log("product found ", product);
     if (!product) {
       return res.status(404).json({ message: "Product not found" });
@@ -152,3 +172,51 @@ router.get("/get/:code", async (req, res) => {
 });
 
 module.exports = router;
+
+async function getProductsByStore(storeId) {
+  try {
+    // Perform aggregation to join products with storeProducts
+    const result = await StoreProductMap.aggregate([
+      { $match: { StoreId: storeId } },
+      {
+        $lookup: {
+          from: "products",
+          localField: "Code",
+          foreignField: "Code",
+          as: "productDetails",
+        },
+      },
+      { $unwind: "$productDetails" },
+      {
+        $project: {
+          _id: "$productDetails._id",
+          StoreId: 1,
+          Quantity: 1,
+          visible: 1,
+          ShopQty: 1,
+          NewArrival: 1,
+          TimesRented: 1,
+          Remarks: 1,
+          Code: "$productDetails.Code",
+          Name: "$productDetails.Name",
+          Description: "$productDetails.Description",
+          MRP: "$productDetails.MRP",
+          Age: "$productDetails.Age",
+          AgeType: "$productDetails.AgeType",
+          Brand: "$productDetails.Brand",
+          Category: "$productDetails.Category",
+          Class: "$productDetails.Class",
+          rent30: "$productDetails.rent30",
+          rent15: "$productDetails.rent15",
+          bigSize: "$productDetails.bigSize",
+          VideoOnInsta: "$productDetails.VideoOnInsta",
+          SearchKey: "$productDetails.SearchKey",
+        },
+      },
+    ]);
+
+    return result;
+  } catch (error) {
+    console.error("Error fetching products by store:", error);
+  }
+}
